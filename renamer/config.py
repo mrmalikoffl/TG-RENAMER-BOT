ec2350732fe7bd631fae0364e7765d54fd7659d3
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2022-12-28 18:01:46 +0800

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 class Config:
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 12345))
     AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS", "") else []
     if OWNER_ID not in AUTH_USERS:
         AUTH_USERS.append(OWNER_ID)
